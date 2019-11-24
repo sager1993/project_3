2800df76686b0def7641d5425243fb11295adea1
aba20 <askr1000@gmail.com>
2019-11-24 11:19:19 +0300

    Bug was fiexed in route file. Pair coded Abdulrahman & Sager

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'static/index'
   get 'static/not_index'
   devise_for :users
-  root to: 'home#index'
+  root to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
