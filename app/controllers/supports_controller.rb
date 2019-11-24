bec0a0f1720141710f30b1581efd7a61cac392b0
aba20 <askr1000@gmail.com>
2019-11-24 14:30:16 +0300

    Changes in support contoller which are add new, create, and destroy

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,2 +1,43 @@
 class SupportsController < ApplicationController
+
+    before_action :is_owner, only: [:destroy]
+    def index 
+        @supports = Support.all
+    end
+
+    def new
+        @support = Support.new
+    end
+
+    def create
+        if user_signed_in?
+        @support = current_user.supports.create(supports_params)
+        if @support.save
+            redirect_to supports_path   
+        else
+              render :new 
+          end
+          else
+            redirect_to new_user_session_path
+          end
+    end
+
+    def destroy
+        Support.find(params[:id]).destroy
+        redirect_to supports_path
+    end
+
+
+    private
+    def supports_params
+        params.require(:support).permit(:body)
+    end
+
+    def is_owner
+        if current_user.id == Support.find(params[:id]).user_id
+            return true
+        else
+            redirect_to supports_path
+        end
+    end
 end
