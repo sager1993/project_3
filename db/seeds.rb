1fba16cf9aa2a08c76c88ef88393bccb3d74b2f5
saera1988 <saera1988@hotmail.com>
2019-11-26 10:04:11 +0300

    edit the database and half the cause view

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,34 +2,34 @@
 
      Cause.create(name: "Raise awareness about human trafficking" ,
      description: "Stop human trafficking right away but to start we need to talk about this issue a lot more and not let it gain dust waiting for someone to discover it themselves. We can do this by talking about it more on the news across the world and inform everyone as much as possible.", 
-     category:"Human Rights ")
+     category:"Human Rights ", user_id: 1)
      Cause.create(name: "Help provide children a future without war!",
      description: "helps provide high-standard free-of-charge medical and surgical care to civilian victims of war and poverty and promotes a culture of peace, solidarity and respect for human rights.",
-     category:"Human Rights ")
+     category:"Human Rights " , user_id: 1 )
      Cause.create(name: "Empower people to live healthier" , 
      description: "Tackling the obstacles to healthy eating starts with making simple changes in the ways we shop, prepare, and cook our meals. Switching to healthier eating doesn't just happen overnight – it begins with small changes ",
-     category:" Health")
+     category:" Health" , user_id: 1)
      Cause.create(name: "Reduce potential cancer-causing ingredient in soda",
      description: "Caramel Coloring, an ingredient that makes your cola brown, may be dangerous to your health.",
-     category:"Health")
+     category:"Health" , user_id: 1)
      Cause.create(name: "Make everyone aware of the effect plastic pollution" , 
      description: "pollution is a growing worldwide issue; as population increases so 
      does the pollution of our oceans. We must much reduce
      plastic pollution in our oceans or better still end
      all careless littering of plastic." ,
-     category:"environment")
+     category:"environment" , user_id: 1)
      Cause.create(name: "Change the way we dispose of rubbish", 
      description: "Landfill is the disposal of waste by burial. 
      The goal of this is to raise awareness to the issue of landfill
      and the negative effects it has on the earth. 
      This campaign is important because people are generally unaware of how bad this 
      can be for all aspects of the earth.",
-     category:"environment")
+     category:"environment" , user_id: 1)
      Cause.create(name: "Bring More Fun and Creative Thought Into the Classroom",
      description: "The main problem in education today is that we're still using 20th century tools and practices while we've already moved into a 21st century environment. " ,
-     category:"education")
+     category:"education" , user_id: 1)
      Cause.create(name: "Full, free and fair bankruptcy protections for student loans" ,
      description: "If you are burdened by predatory, exploitative private student loans, or federal student loans, you must sign this petition and spread it to as many people as you possibly can." ,
-     category:"education")
+     category:"education" , user_id: 1)
      
      Support.create(cause_id: 5, user_id: 1, title: "Save Turtels", body: "Turtels in the sea are paying the price for dumping our trash into oceans. Me and you should contibute to reduce trash dumping.")
